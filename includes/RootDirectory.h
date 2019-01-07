10516e03e09900532118ba6f829db9ed81988fd5
Marius Butz <marius@localhost.localdomain>
2019-01-07 10:26:17 +0100

    Fixed bugs in RootDirectory

--- includes/RootDirectory.h
+++ includes/RootDirectory.h
@@ -14,7 +14,12 @@ class RootDirectory {
 	public:
 		RootDirectory();
 		~RootDirectory();
-		int addEntry(const char* path, mode_t mode, uint8_t uid, uint8_t gid);
+		int addEntry(const char* path, uint16_t firstblock, uint32_t sizeOfFile, mode_t mode, uint8_t uid, uint8_t gid);
+		/**
+		 * return index of fileentry matching path, negative int when error occured
+		 */
+		int searchEntry(const char* path, uint8_t uid, uint8_t gid);
+		FileEntry getEntry(int index);
 		int removeEntry(const char* path);
 		void serialize();
 		void deserialize();
--- src/RootDirectory.cpp
+++ src/RootDirectory.cpp
@@ -1,4 +1,5 @@
-#include "../includes/RootDirectory.h"
+#include "RootDirectory.h"
+#include <iostream>
 
 RootDirectory::RootDirectory() {
 	this->listDeserialized = false;
@@ -9,43 +10,36 @@ RootDirectory::~RootDirectory() {
 	this->serialize();
 }
 
-int RootDirectory::addEntry(const char* path, mode_t mode, uint8_t uid, uint8_t gid) {
-	FileEntry newEntry;
+int RootDirectory::addEntry(const char* path, uint16_t firstblock, uint32_t sizeOfFile, mode_t mode, uint8_t uid, uint8_t gid) {
+	FileEntry *newEntry = new FileEntry;
 	//Remove Path information of filepath
-	int i = 0;
-	int lastIndex = 0;
-	while (*path != '\0') {
-		if (*path == '/' || *path == '\\' ){
-			lastIndex = i;
-		}
-		i++;
-		path++;
-	}
-	path -= lastIndex;
+	char *escapedpath = strrchr(strdup(path), '/');
 	
 	//check if file already exists
 	for (int i = 0; i < DATA_BLOCKS; i++) {
-		if (strcmp(this->fileList[i].filename, path)) {
+		if (strcmp(this->fileList[i].filename, escapedpath) == 0) {
 			return -(EEXIST);
 		}
 	}
 
 	//write entry
-	newEntry.filename = path;
-	newEntry.mode = mode;
+	strcpy(newEntry->filename, escapedpath);
+	newEntry->mode = mode;
 
 	struct timeval tv;
 	gettimeofday(&tv,nullptr);
-	newEntry.atime = (uint32_t) tv.tv_sec;
-	newEntry.ctime = (uint32_t) tv.tv_sec;
-	newEntry.mtime = (uint32_t) tv.tv_sec;
-	newEntry.gid = gid;
-	newEntry.uid = uid;
+	newEntry->atime = (uint32_t) tv.tv_sec;
+	newEntry->ctime = (uint32_t) tv.tv_sec;
+	newEntry->mtime = (uint32_t) tv.tv_sec;
+	newEntry->gid = gid;
+	newEntry->uid = uid;
+	newEntry->firstBlock = firstblock;
+	newEntry->sizeOfFile = sizeOfFile;
 
 	//save entry in list
 	for (int i = 0; i < DATA_BLOCKS; i++) {
-		if (this->fileList[i] == 0) {
-			this->fileList[i] = newEntry;
+		if (this->fileList[i].firstBlock == 0) {
+			this->fileList[i] = *newEntry;
 			i = DATA_BLOCKS;
 			return 1;
 		}
@@ -53,4 +47,31 @@ int RootDirectory::addEntry(const char* path, mode_t mode, uint8_t uid, uint8_t 
 
 	return -(EIO);
 	
+}
+
+int RootDirectory::searchEntry(const char* path, uint8_t uid, uint8_t gid){
+	for(int i = 0; i < DATA_BLOCKS; i++) {
+		if(strcmp(this->fileList[i].filename, path) == 0){
+			return i;
+		}
+	}
+	return -(ENOENT);
+}
+
+FileEntry RootDirectory::getEntry(int index){
+	if(index >= 0){
+		return this->fileList[index];
+	}
+	else{
+		FileEntry *fe = new FileEntry;
+		return *fe;
+	}
+}
+
+void RootDirectory::serialize(){
+	return;
+}
+
+void RootDirectory::deserialize(){
+	return;
 }
