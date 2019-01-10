af82d7d912916c26a0cd18da9fe555c7ffb433dd
THaenlein <tim_24398@gmx.de>
2019-01-10 11:49:26 +0100

    added Superblock

---
+++ src/Superblock.cpp
@@ -0,0 +1,57 @@
+#include "Superblock.h"
+
+/*
+Finde den nächsten freien Block in der DMap und gib die Adresse dessen zurück.
+*/
+uint16_t Superblock::findFreeBlock()
+{
+	uint16_t i = 1;
+	while(DMap[i]) i++;
+	return FAT[i];
+}
+
+/*
+Hat der Angegebene Block einen nachfolge Block? Falls ja 
+gib die Adresse zurück, falls nein gib 0 zurück.
+*/
+uint16_t Superblock::findNextBlock(uint16_t address)
+{
+	if(FAT[adress]) return (FAT[adress];
+		else return false;
+}
+
+/*
+Setzt den nächsten Block eines Blocks. Um den nachfolgenden 
+Block zu entfernen muss nextBlockAdress auf -1 gesetzt wird.
+*/
+void Superblock::setNextBlock(uint16_t blockAdress, uint16_t nextBlockAddress)
+{
+	if (blockAdress < 1) return;
+	if(blockAdress == -1) FAT[blockAdress] = 0;
+		else FAT[blockAdress] = nextBlockAddress;
+}
+
+/*
+Markiere Block als belegt (status = false) oder frei (status = true)
+*/
+void Superblock::markBlock(uint16_t address, Boolean status)
+{
+	if (address) DMap[adress] = status;
+}
+
+/*
+Serialisiere den gesamten Superblock (bestehend aus DMAP, FAT und ggf. weiteren Attributen) 
+sequentiell in den Buffer, welcher anschließend geschrieben werden kann.
+*/
+void Superblock::serialize(const char* buffer)
+{
+    
+}
+
+/*
+Lese den Buffer ein und schreibe die einzelnen Elemente zurück in die Attribute des Superblocks.
+*/
+void Superblock::deserialize(const char* buffer)
+{
+    
+}
