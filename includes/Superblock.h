5f91743d2f53c4ae58b9fd2de03bccf4fc02677f
THaenlein <40724414+THaenlein@users.noreply.github.com>
2019-01-12 13:48:12 +0100

    updated Superblock

--- includes/Superblock.h
+++ includes/Superblock.h
@@ -2,20 +2,30 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 #ifndef Superblock_h
 #define Superblock_h
 
 #define OFFSET_ROOT 272
 #define OFFSET_DATA 4880
 #define OFFSET_ENDOFDATA 70416
+#define DMAP_SIZE 65536
+#define FAT_SIZE 65536
 
 class Superblock {
 
-private:
-	uint32_t blockCount;
-	bool DMap[65536];
-	uint16_t FAT[65536];
 public:
+	uint32_t blockCount;
+	bool DMap[DMAP_SIZE];
+	uint16_t FAT[FAT_SIZE];
+//public:
+
+	Superblock();
+
+	Superblock(bool dmap[], uint16_t fat[]);
+
+	~Superblock();
 
 	uint16_t findFreeBlock();
 
@@ -25,15 +35,16 @@ public:
 
 	void markBlock(uint16_t adress, bool status);
 
-	void serialize();
+	void serialize(char* buffer);
 
-	void deserialize();
+	void deserialize(char* buffer);
 };
-
+/*
 typedef struct
 {
-	bool DMap[65536];
-	uint16_t FAT[65536];
+	bool DMap[DMAP_SIZE];
+	uint16_t FAT[FAT_SIZE];
 }SerializedSuperBlock;
+*/
 
 #endif // Superblock_h
--- src/Superblock.cpp
+++ src/Superblock.cpp
@@ -1,31 +1,35 @@
 #include "Superblock.h"
 
 
-/*
-Superblock()
+Superblock::Superblock(bool dmap[], uint16_t fat[])
 {
-	this->deserialize();
+	this->DMap = dmap;
+
 }
 
-~Superblock()
+Superblock::Superblock()
 {
-	this->serialize();
+
 }
-*/
+
+Superblock::~Superblock()
+{
+
+}
 
 /*
-Finde den nÃ¤chsten freien Block in der DMap und gib die Adresse dessen zurÃ¼ck.
+Finde den nächsten freien Block in der DMap und gib die Adresse dessen zurück.
 */
 uint16_t Superblock::findFreeBlock()
 {
 	uint8_t i = 1;
 	while (DMap[i]) i++;
-	return FAT[i];
+	return i;
 }
 
 /*
 Hat der Angegebene Block einen nachfolge Block? Falls ja
-gib die Adresse zurÃ¼ck, falls nein gib 0 zurÃ¼ck.
+gib die Adresse zurück, falls nein gib 0 zurück.
 */
 uint16_t Superblock::findNextBlock(uint16_t address)
 {
@@ -34,13 +38,13 @@ uint16_t Superblock::findNextBlock(uint16_t address)
 }
 
 /*
-Setzt den nÃ¤chsten Block eines Blocks. Um den nachfolgenden
+Setzt den nächsten Block eines Blocks. Um den nachfolgenden
 Block zu entfernen muss nextBlockAdress auf -1 gesetzt wird.
 */
 void Superblock::setNextBlock(uint16_t blockAddress, uint16_t nextBlockAddress)
 {
-	if (blockAddress < 1) return;
-	if (blockAddress == -1) FAT[blockAddress] = 0;
+	if (blockAddress == 0) return;
+	if (nextBlockAddress == -1) FAT[blockAddress] = 0;
 	else FAT[blockAddress] = nextBlockAddress;
 }
 
@@ -54,23 +58,30 @@ void Superblock::markBlock(uint16_t address, bool status)
 
 /*
 Serialisiere den gesamten Superblock (bestehend aus DMAP, FAT und ggf. weiteren Attributen)
-sequentiell in den Buffer, welcher anschlieÃŸend geschrieben werden kann.
+sequentiell in den Buffer, welcher anschließend geschrieben werden kann.
 */
-void Superblock::serialize()
+void Superblock::serialize(char* buffer)
 {
+	/*
 	SerializedSuperBlock s = { this->DMap, this->FAT };
-	//printf("Serialize: %d", sizeof(s));
-	char* buffer = new char[sizeof(s)];
+	delete buffer;
+	//buffer = (char*) malloc(sizeof(s));
+	buffer = new char[sizeof(s)];
 	memcpy(&buffer, &s, sizeof(s));
+	*/
+
+	delete buffer;
+	//buffer = (char*) malloc(sizeof(this->DMap) + sizeof(this->FAT));
+	buffer = new char[sizeof(this->DMap) + sizeof(this->FAT)];
+	memcpy(buffer, DMap, sizeof(DMap));
+	memcpy(buffer + sizeof(DMap), FAT, sizeof(FAT));
 }
 
 /*
-Lese den Buffer ein und schreibe die einzelnen Elemente zurÃ¼ck in die Attribute des Superblocks.
+Lese den Buffer ein und schreibe die einzelnen Elemente zurück in die Attribute des Superblocks.
 */
-void Superblock::deserialize()
+void Superblock::deserialize(char* buffer)
 {
-	SerializedSuperBlock s;
-	char* buffer = new char[sizeof(s)];
-	memcpy(&s, &buffer, sizeof(s));
-	//this.DMap = s.DMap;
+	memcpy(DMap, buffer, sizeof(buffer) - sizeof(this->FAT));
+	memcpy(FAT, buffer + sizeof(DMap), sizeof(this->FAT));
 }
