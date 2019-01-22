bd2bd070ab248cd72653463b98102bbc88551873
Marius Butz <mbpictures13@gmail.com>
2019-01-22 21:06:32 +0100

    fix write :)

--- src/myfs.cpp
+++ src/myfs.cpp
@@ -294,7 +294,7 @@ int MyFS::fuseWrite(const char *path, const char *buf, size_t size, off_t offset
  		j++;
  		readIn++;
 
- 		if(readIn == (BLOCK_SIZE)){
+ 		if(readIn == (BLOCK_SIZE) || j == size){
  		    this->bd->write(currentWriteBlock + DATA_START_BLOCK - 1, bufferWrite);
  		    readIn = 0;
  		    //LOGF("Write Block: %d", currentWriteBlock + DATA_START_BLOCK - 1);
